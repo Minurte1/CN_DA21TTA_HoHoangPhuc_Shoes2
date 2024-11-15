e73d33f45ae9d31973c71cd65774f517c3ee7d54
Minurte1 <hohoangphucjob@gmail.com>
2024-11-15 13:41:51 +0700

    update

--- react-phucshoe2/src/web-view/view-page/browseShoes.jsx
+++ react-phucshoe2/src/web-view/view-page/browseShoes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Grid,
   Card,
@@ -11,36 +11,31 @@ import {
   FormControl,
   Drawer,
   IconButton,
+  Box,
 } from "@mui/material";
 import { FilterList } from "@mui/icons-material";
-const products = [
-  {
-    title: "VED",
-    price: "₫256,800",
-    category: "new",
-    image:
-      "https://cdn1.epicgames.com/spt-assets/9ab7eaaeb2f041969b350955b789f0f7/sorry-were-closed-1xagz.jpg?resize=1&w=360&h=480&quality=medium",
-  },
-  {
-    title: "Necroking",
-    price: "₫73,500",
-    category: "new",
-    image:
-      "https://cdn1.epicgames.com/spt-assets/9ab7eaaeb2f041969b350955b789f0f7/sorry-were-closed-1xagz.jpg?resize=1&w=360&h=480&quality=medium",
-  },
-  {
-    title: "Sorry We’re Closed",
-    price: "₫261,000",
-    category: "popular",
-    image:
-      "https://cdn1.epicgames.com/spt-assets/9ab7eaaeb2f041969b350955b789f0f7/sorry-were-closed-1xagz.jpg?resize=1&w=360&h=480&quality=medium",
-  },
-  // Add more product data here
-];
+import axios from "axios";
+import ProductCarousel from "../../share-view/productCarousel";
+const api = process.env.REACT_APP_URL_SERVER;
 
 const BrowseProduct = () => {
   const [filter, setFilter] = useState("");
   const [mobileOpen, setMobileOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const fetchProduct = async (id) => {
+    try {
+      const response = await axios.get(`${api}/san-pham/use`);
+      if (response.data.EC === 1) {
+        setProducts(response.data.DT); // Set product data
+      }
+    } catch (error) {
+      console.error("Error fetching product:", error);
+    }
+  };
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
@@ -51,69 +46,89 @@ const BrowseProduct = () => {
   );
 
   return (
-    <div style={{ display: "flex" }}>
-      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-        {/* Product Grid */}
-        <Grid item xs={12} sm={9} container spacing={2}>
-          {filteredProducts.map((product, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={product.image} // Replace with product image URL
-                  alt={product.title}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {product.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {product.price}
-                  </Typography>
-                </CardContent>
-              </Card>
+    <>
+      <Box>
+        <div>
+          <ProductCarousel api={api} products={products} />
+        </div>
+
+        <div style={{ display: "flex" }}>
+          <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+            {/* Product Grid */}
+            <Grid item xs={12} sm={9} container spacing={2}>
+              {filteredProducts.map((product, index) => (
+                <Grid item xs={12} sm={6} md={3} key={index}>
+                  <Card>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={`${api}/images/${product.HINH_ANH_SANPHAM}`}
+                      alt={product.TEN_SAN_PHAM}
+                      sx={{
+                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh được hiển thị
+                        display: "block", // Đặt display là block
+                        margin: "0 auto", // Căn giữa hình ảnh theo chiều ngang
+                      }}
+                    />
+
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {product.TEN_SAN_PHAM}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {product.MO_TA_SAN_PHAM}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
             </Grid>
-          ))}
-        </Grid>
 
-        {/* Desktop Filter Sidebar on Right */}
-        <Grid item xs={12} sm={3} display={{ xs: "none", sm: "block" }}>
-          <SidebarFilter filter={filter} onFilterChange={handleFilterChange} />
-        </Grid>
+            {/* Desktop Filter Sidebar on Right */}
+            <Grid item xs={12} sm={3} display={{ xs: "none", sm: "block" }}>
+              <SidebarFilter
+                filter={filter}
+                onFilterChange={handleFilterChange}
+              />
+            </Grid>
 
-        {/* Drawer for Mobile Filter Sidebar */}
-        <Drawer
-          variant="temporary"
-          anchor="right" // Mở Drawer từ bên phải
-          open={mobileOpen}
-          onClose={() => setMobileOpen(false)}
-          ModalProps={{ keepMounted: true }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 },
-          }}
-        >
-          <SidebarFilter filter={filter} onFilterChange={handleFilterChange} />
-        </Drawer>
-      </Grid>
+            {/* Drawer for Mobile Filter Sidebar */}
+            <Drawer
+              variant="temporary"
+              anchor="right" // Mở Drawer từ bên phải
+              open={mobileOpen}
+              onClose={() => setMobileOpen(false)}
+              ModalProps={{ keepMounted: true }}
+              sx={{
+                display: { xs: "block", sm: "none" },
+                "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 },
+              }}
+            >
+              <SidebarFilter
+                filter={filter}
+                onFilterChange={handleFilterChange}
+              />
+            </Drawer>
+          </Grid>
 
-      {/* Mobile Filter Button */}
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="end" // Căn bên phải cho nút filter trên mobile
-        onClick={() => setMobileOpen(true)}
-        sx={{
-          display: { sm: "none" },
-          position: "fixed",
-          bottom: 16,
-          right: 16,
-        }}
-      >
-        <FilterList />
-      </IconButton>
-    </div>
+          {/* Mobile Filter Button */}
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end" // Căn bên phải cho nút filter trên mobile
+            onClick={() => setMobileOpen(true)}
+            sx={{
+              display: { sm: "none" },
+              position: "fixed",
+              bottom: 16,
+              right: 16,
+            }}
+          >
+            <FilterList />
+          </IconButton>
+        </div>
+      </Box>
+    </>
   );
 };
 
